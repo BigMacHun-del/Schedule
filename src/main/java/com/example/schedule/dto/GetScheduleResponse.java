3e343f87505f576438111ffc2b6d3f78884af63c
김대훈 <eogns136@gmail.com>
2025-12-30 12:11:37 +0900

    Feat: 일정 전체 조회 기능 추가 완료

---
+++ src/main/java/com/example/schedule/dto/GetScheduleResponse.java
@@ -0,0 +1,25 @@
+package com.example.schedule.dto;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+@Getter
+public class GetScheduleResponse {
+    private final Long scheduleId;
+    private final String title;
+    private final String content;
+    private final String name;
+    private final Date createDate;
+    private final Date updateDate;
+
+    public GetScheduleResponse(Long scheduleId, String title, String content, String name, Date createDate, Date updateDate) {
+        this.scheduleId = scheduleId;
+        this.title = title;
+        this.content = content;
+        this.name = name;
+        this.createDate = createDate;
+        this.updateDate = updateDate;
+    }
+}
