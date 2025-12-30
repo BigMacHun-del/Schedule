e54fd4969ff7f5b130caa26c69340be0484dc419
김대훈 <eogns136@gmail.com>
2025-12-30 10:54:30 +0900

    Feat: 일정 생성 Service, DTO 생성 완료

---
+++ src/main/java/com/example/schedule/dto/CreateScheduleResponse.java
@@ -0,0 +1,24 @@
+package com.example.schedule.dto;
+
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+public class CreateScheduleResponse {
+    private final Long scheduleId;
+    private final String title;
+    private final String content;
+    private final String name;
+    private final Date createDate;
+    private final Date updateDate;
+
+    public CreateScheduleResponse(Long scheduleId, String title, String content, String name, Date createDate, Date updateDate) {
+        this.scheduleId = scheduleId;
+        this.title = title;
+        this.content = content;
+        this.name = name;
+        this.createDate = createDate;
+        this.updateDate = updateDate;
+    }
+}
