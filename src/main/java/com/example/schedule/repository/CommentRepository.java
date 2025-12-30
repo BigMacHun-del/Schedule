3db121c2dc2bfca80bf7929ec9c8d6faacd57a26
김대훈 <eogns136@gmail.com>
2025-12-30 19:09:06 +0900

    Feat: 10개  초과 댓글이 달리면 댓글 쓰기 불가 기능 구현

--- src/main/java/com/example/schedule/repository/CommentRepository.java
+++ src/main/java/com/example/schedule/repository/CommentRepository.java
@@ -4,4 +4,5 @@ import com.example.schedule.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    int countByScheduleId(Long scheduleId);  //댓글 데이터베이스에 있는 scheduleId와 같은 값을 가진 필드 개수 카운트
 }
--- src/main/java/com/example/schedule/service/CommentService.java
+++ src/main/java/com/example/schedule/service/CommentService.java
@@ -19,8 +19,15 @@ public class CommentService {
     //댓글 생성
     @Transactional
     public CreateCommentResponse save(Long scheduleId, CreateCommentRequest request){
+        // 일정이 있는지 확인
         Schedule schedule = scheduleRepository.findById(scheduleId)
                 .orElseThrow(() -> new IllegalArgumentException("없는 일정입니다."));
+
+        // 댓글 데이터베이스에 있는 scheduleId 값을 세서 10이 넘으면 예외를 던져줌
+        int commentCount = commentRepository.countByScheduleId(scheduleId);
+        if (commentCount >= 10) {
+            throw new IllegalStateException("하나의 일정에는 댓글을 10개까지만 작성할 수 있습니다.");
+        }
 
         Comment comment = new Comment(
                 request.getCommentContent(),
