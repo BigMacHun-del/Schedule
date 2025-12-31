babdadf35eb130ba69cb089a4de24a139d67863b
김대훈 <eogns136@gmail.com>
2025-12-31 10:36:04 +0900

    Chore: 주석 추가, 수정

--- src/main/java/com/example/schedule/repository/CommentRepository.java
+++ src/main/java/com/example/schedule/repository/CommentRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    int countByScheduleId(Long scheduleId);  //댓글 데이터베이스에 있는 scheduleId와 같은 값을 가진 필드 개수 카운트
+    //댓글 데이터베이스에 있는 scheduleId와 같은 값을 가진 필드 개수 카운트
+    int countByScheduleId(Long scheduleId);
 
+    //일정 아이디 찾기(댓글 테이블에 있는 scheduleId와 매핑하는 역할)
     List<Comment> findByScheduleId (Long scheduleId);
 }
